9ea709c8e43374fe26f369413f862bd054479489
hackabean <50614334+hackabean@users.noreply.github.com>
2019-08-18 11:09:44 +0100

    Delete javascript_base64_decode.js

--- javascript_base64_decode.js
+++
@@ -1,5 +0,0 @@
-var fso = new ActiveXObject("Scripting.FileSystemObject");
-var b = fso.CreateTextFile("C:\\Windows\\Temp\\base64", true);
-b.WriteLine("base64_encoded_file");
-b.Close();
-var go = new ActiveXObject("Wscript.Shell").Run("certutil -decode C:\\Windows\\Temp\\base64 C:\\Windows\\Temp\\file.*",0);
